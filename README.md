39717d04a4c60cba3f50d5442d11e96ef9e1a73f
Gee <alickoamarko@gmail.com>
2026-01-03 14:20:22 +0100

    MVP?

---
+++ README.md
@@ -0,0 +1,65 @@
+General-purpose cross-platform networked state synchronization server
+
+
+# Protocol
+
+After first packet received, server sends:
+```
+i64 client_id
+```
+
+For all subsequent communication (both send and receive):
+```
+i64 packet_num, str payload
+```
+(`payload` is an `{int: str}` dictionary where the keys are `client_id`'s)
+
+(`packet_num` is just a number which is incremented for each new packet)
+
+
+# Custom client example
+```py
+import socket
+import json
+import threading
+from time import sleep
+
+
+client_states = {}
+
+s = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+s.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
+
+def receive_handler():
+        global s
+        global client_states
+        server_packet_seq_num = -1
+        while True:
+                data, _ = s.recvfrom(65536)
+                if int.from_bytes(data[:8]) <= server_packet_seq_num: continue
+                server_packet_seq_num = int.from_bytes(data[:8])
+                client_states = json.loads(data[8:].decode())
+
+local_client_state = 0
+local_packet_seq_num = -1
+local_client_id = -1
+
+s.sendto(bytes(1), ("::1", 55555))
+data, _ = s.recvfrom(65536)
+local_client_id = int.from_bytes(data)
+
+threading.Thread(target=receive_handler).start()
+
+while True:
+        print(client_states)
+        local_client_state += 1
+        local_packet_seq_num += 1
+        s.sendto(
+                (
+                        local_packet_seq_num.to_bytes(8) +
+                        str(local_client_state).encode()
+                ),
+                ("::1", 55555)
+        )
+        sleep(0.1)
+```
---
+++ SimpleNetSyncServer.py
@@ -0,0 +1,71 @@
+# Config
+
+DEFAULT_PORT = 55555
+DEFAULT_MAX_CLIENTS = 32
+
+
+
+
+# Implementation
+
+import sys
+import socket
+import signal
+import json
+from typing import Any
+
+
+_guid = -1
+def new_GUID() -> int:
+        global _guid
+        _guid += 1
+        return _guid
+
+
+port = DEFAULT_PORT
+if len(sys.argv) >= 2:
+        port = int(sys.argv[1])
+
+max_clients = DEFAULT_MAX_CLIENTS
+if len(sys.argv) >= 3:
+        max_clients = int(sys.argv[2])
+
+print(f"Port: {port}\nMax clients: {max_clients}")
+
+clients: list[(str, int)] = []
+client_ids: dict[(str, int), int] = {}
+client_packet_seq_numbers: dict[int, int] = {}
+client_states: dict[int, Any] = {}
+
+packet_seq_number = 0
+
+s = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+s.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
+s.bind(("::", port))
+keep_running = True
+signal.signal(signal.SIGINT, lambda: (keep_running := False))
+print("Server started")
+while keep_running:
+        data, addr = s.recvfrom(65536)
+        if addr not in clients:
+                clients.append(addr)
+                client_ids[addr] = new_GUID()
+                client_packet_seq_numbers[client_ids[addr]] = -1
+                packet_seq_number += 1
+                s.sendto(client_ids[addr].to_bytes(8), addr)
+                print(f"Client {client_ids[addr]} connected")
+                continue
+        data_packet_seq = int.from_bytes(data[:8])
+        if data_packet_seq <= client_packet_seq_numbers[client_ids[addr]]:
+                continue
+        client_packet_seq_numbers[client_ids[addr]] = data_packet_seq
+        client_states[client_ids[addr]] = data[8:].decode()
+        packet_seq_number += 1
+        for client in clients:
+                s.sendto(
+                        (
+                                packet_seq_number.to_bytes(8) +
+                                json.dumps(client_states).encode()
+                        ),
+                        client
+                )
