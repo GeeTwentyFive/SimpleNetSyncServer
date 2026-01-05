0bbc9d98353c03256240218b1993f4587e203191
Gee <alickoamarko@gmail.com>
2026-01-05 18:15:21 +0100

    ID receive verification, connection, and timeout

--- README.md
+++ README.md
@@ -11,63 +11,26 @@ GDScript client library: https://github.com/GeeTwentyFive/GodotSimpleNetSync
 
 # Protocol
 
-After first packet received, server sends:
+Client keeps sending packet `-1` (i64) until server replies with `-1` (i64) + `id`
+
+After receiving `-1` (i64), server sends:
 ```
-i64 client_id
+i64 -1, i64 client_id
 ```
+(`client_id` is ID of that client. Used as a key in states dict)
+
+(Then, the server must receive at least one regular communication packet from the client for the client to be considered "connected" and start receiving the states of all clients)
 
 For all subsequent communication (both send and receive):
 ```
 i64 packet_num, str payload
 ```
-(`payload` is an `{int: str}` dictionary where the keys are `client_id`'s)
+(From server to client: `payload` is an `{int: str}` dictionary of client states where the keys are `client_id`'s)
+
+(From client: `payload` is placed inside client states dict on server at key `client_id` (already associated with client (based on address) on server))
 
 (`packet_num` is just a number which is incremented for each new packet)
 
 
 # Custom client example
-```py
-import socket
-import json
-import threading
-from time import sleep
-
-
-client_states = {}
-
-s = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
-s.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
-
-def receive_handler():
-        global s
-        global client_states
-        server_packet_seq_num = -1
-        while True:
-                data, _ = s.recvfrom(65536)
-                if int.from_bytes(data[:8]) <= server_packet_seq_num: continue
-                server_packet_seq_num = int.from_bytes(data[:8])
-                client_states = json.loads(data[8:].decode("ascii"))
-
-local_client_state = 0
-local_packet_seq_num = -1
-local_client_id = -1
-
-s.sendto(bytes(1), ("::1", 55555))
-data, _ = s.recvfrom(65536)
-local_client_id = int.from_bytes(data)
-
-threading.Thread(target=receive_handler).start()
-
-while True:
-        print(client_states)
-        local_client_state += 1
-        local_packet_seq_num += 1
-        s.sendto(
-                (
-                        local_packet_seq_num.to_bytes(8) +
-                        str(local_client_state).encode("ascii")
-                ),
-                ("::1", 55555)
-        )
-        sleep(0.1)
-```
+TODO
--- SimpleNetSyncServer.py
+++ SimpleNetSyncServer.py
@@ -1,6 +1,7 @@
 # Config
 
 DEFAULT_PORT = 55555
+TIMEOUT = 10.0
 
 
 
@@ -10,8 +11,12 @@ DEFAULT_PORT = 55555
 import sys
 import socket
 import signal
+import time
 import json
 from typing import Any
+
+
+CONNECT_REQUEST_PACKET = -1
 
 
 _guid = -1
@@ -25,8 +30,10 @@ port = DEFAULT_PORT
 if len(sys.argv) >= 2:
         port = int(sys.argv[1])
 
+unverified_clients: list[(str, int)] = []
 clients: list[(str, int)] = []
 client_ids: dict[(str, int), int] = {}
+client_last_packet_times: dict[int, float] = {}
 client_packet_seq_numbers: dict[int, int] = {}
 client_states: dict[int, Any] = {}
 
@@ -40,19 +47,46 @@ signal.signal(signal.SIGINT, lambda: (keep_running := False))
 print(f"Server started on port {port}")
 while keep_running:
         data, addr = s.recvfrom(65536)
+
+        # Handle non-existing-client packets
         if addr not in clients:
-                clients.append(addr)
-                client_ids[addr] = new_GUID()
-                client_packet_seq_numbers[client_ids[addr]] = -1
-                packet_seq_number += 1
-                s.sendto(client_ids[addr].to_bytes(8, "little"), addr)
-                print(f"Client {client_ids[addr]} connected")
+                if addr not in unverified_clients:
+                        if int.from_bytes(data[:8], "little") != CONNECT_REQUEST_PACKET: continue
+                        # New connection attempt
+                        unverified_clients.append(addr)
+                        client_ids[addr] = new_GUID()
+                        s.sendto(client_ids[addr].to_bytes(8, "little"), addr)
+                else:
+                        if int.from_bytes(data[:8], "little") == CONNECT_REQUEST_PACKET: continue
+                        # Connection verified, add as client
+                        unverified_clients.remove(addr)
+                        clients.append(addr)
+                        client_last_packet_times[client_ids[addr]] = time.monotonic()
+                        print(f"Client {client_ids[addr]} connected")
+                        # (save received client state)
+                        client_packet_seq_numbers[client_ids[addr]] = int.from_bytes(data[:8], "little")
+                        client_states[client_ids[addr]] = data[8:].decode("ascii")
                 continue
+
+        # Handle timeouts
+        for client in clients:
+                if (
+                        time.monotonic() -
+                        client_last_packet_times[client_ids[client]]
+                ) > TIMEOUT:
+                        clients.remove(client)
+                        print(f"Client {client_ids[client]} disconnected")
+        
+        client_last_packet_times[client_ids[addr]] = time.monotonic()
+
+        # Save received client state
         data_packet_seq = int.from_bytes(data[:8], "little")
         if data_packet_seq <= client_packet_seq_numbers[client_ids[addr]]:
                 continue
         client_packet_seq_numbers[client_ids[addr]] = data_packet_seq
         client_states[client_ids[addr]] = data[8:].decode("ascii")
+
+        # Sync client states
         packet_seq_number += 1
         for client in clients:
                 s.sendto(
